485c20dbd22608441227d11513caa8620375884f
niemha3 <harri.nieminen22@gmail.com>
2022-07-06 14:25:27 +0300

    Päivämäärällä metodin haku valmis

--- TapahtumaApi/TapahtumaApi/HarrinMetodi.cs
+++ TapahtumaApi/TapahtumaApi/HarrinMetodi.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static TapahtumaApi.Program;
 using System.Linq;
+using System.Threading;
 
 namespace TapahtumaApi
 {
@@ -11,30 +12,66 @@ namespace TapahtumaApi
     {
     public static async Task HarrinMetodi() 
     {
-            Console.WriteLine("Anna päivämäärä tapahtumalle esim.(01/01/2000): ");
-            string inputDate = Console.ReadLine();
-            DateTime eventDate = DateTime.Parse(inputDate);
-           
+            DateTime eventDate;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Anna päivämäärä tapahtumalle esim.(01/01/2000): ");
+                    string inputDate = Console.ReadLine();
+                    eventDate = DateTime.Parse(inputDate);
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Syötä päivämäärä oikeassa muodossa pp/kk/vvvv");
+                    continue;
+                }
+                break;
+            }
           
             const string url = "http://open-api.myhelsinki.fi/v1/events/";
             string urlParams = "";
-
+           
+            Console.Write("Haetaan, odota hetki");
+            ShowDots();
             Eventlist tapahtumaList = await ApiHelper.RunAsync<Eventlist>(url, urlParams);
 
             var tapahtumatByDate = tapahtumaList.data.Where(date => date.event_dates.starting_day >= eventDate && date.event_dates.starting_day < eventDate.AddDays(1));
 
-            foreach (var item in tapahtumatByDate)
+            while (true)
             {
-                Console.WriteLine(" " + item.name.fi);
-                Console.WriteLine("\n " + item.description.intro);
-                Console.WriteLine("\n " + item.event_dates.starting_day);
-                Console.WriteLine("\n " + item.location.address.street_address);
+
+                try
+                {
+                    foreach (var item in tapahtumatByDate)
+                    {
+                        Console.WriteLine(" " + item.name.fi);
+                        Console.WriteLine("\n " + item.description.intro);
+                        Console.WriteLine("\n " + item.event_dates.starting_day);
+                        Console.WriteLine("\n " + item.location.address.street_address);
 
 
-                Console.WriteLine("*************************************************************************************************************************");
+                        Console.WriteLine("*************************************************************************************************************************");
+                    }
+
+                }
+
+            
+                catch (NullReferenceException)
+                {
+
+                    Console.WriteLine("Haetulla päivämäärällä ei löytynyt tapahtumia.");
+                    continue;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Jotain meni pieleen.");
+                    continue;
+
+                }
+                break;
             }
-
-
             //foreach (Datum d in tapahtumaList.data)
             //{
             //    Console.WriteLine(d.name.fi + " " + d.event_dates.starting_day);
@@ -45,9 +82,18 @@ namespace TapahtumaApi
             //Console.WriteLine(tapahtuma2.modified_at.Ticks.ToString());
             //Console.WriteLine(tapahtuma2.opening_hours.hours[1].weekday_id.ToString());
 
+        }
 
-
-
+        static void ShowDots()
+        {
+            int i = 0;
+            while (i < 3)
+            {
+                Console.Write(".");
+                Thread.Sleep(500);
+                i++;
+            }
+            Console.WriteLine("\n\n");
         }
 
     }
